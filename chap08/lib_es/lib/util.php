614edb8c586086555da9b912bd0694c96470bef7
SeiichiN <billie175@gmail.com>
2023-10-20 10:57:07 +0900

    am11:00

--- chap08/lib_es/lib/util.php
+++ chap08/lib_es/lib/util.php
@@ -3,7 +3,7 @@ function es(array|string $data, string $charset='UTF-8'):mixed {
   if (is_array($data)) {
     return array_map(__METHOD__, $data);
   } else {
-    return htmlspecialchars($data, ENT_QUOTES, $charset);
+    return htmlspecialchars($data, ENT_QUOTES|ENT_HTML5, $charset);
   }
 }
 
